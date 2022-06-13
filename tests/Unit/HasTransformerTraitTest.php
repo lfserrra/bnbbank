3d768089986984383293ed9a3dc081a8437d8a17
Luis Fernando Serra <lfserrra@gmail.com>
2022-06-12 21:25:29 -0300

    created HasTransformerTraitTest

---
+++ tests/Unit/HasTransformerTraitTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use Turnover\Traits\HasTransformerTrait;
+
+class HasTransformerTraitTest extends TestCase {
+
+    use HasTransformerTrait;
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_has_transformer_trait_without_content()
+    {
+        $this->assertNull($this->applyTransform(null, false));
+        $this->assertEquals(['data' => []], $this->applyTransform(null));
+    }
+}
